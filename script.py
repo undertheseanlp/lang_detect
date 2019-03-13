422fc8dc49e72f900e3beda20108aa696f1ad432
Vu Anh <anhv.ict91@gmail.com>
2019-03-13 09:14:39 +0700

    load fasttext identification

---
+++ script.py
@@ -0,0 +1,6 @@
+from fastText import FastText
+
+ft = FastText.load_model("lid.176.ftz")
+text = 'OpenAI has moved most of its staff to a for-profit LLC. RIP "open-source". RIP "non-profit".'
+labels = ft.predict(text, 3)
+print(labels)
